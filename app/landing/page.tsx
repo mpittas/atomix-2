9a58f7cbd279f58163a8efa209e990a884eb4878
mpittas <webpittas@gmail.com>
2026-04-07 16:54:19 +0300

    Add aurora backgrounds to sections

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -300,7 +300,26 @@ export default function LandingGradientV1Page() {
         </div>
 
         <div className="h-full mb-6">
-          <div className="px-18 py-36 rounded-t-3xl bg-linear-to-b from-[#0B4858] to-[#81A6AF]">
+          <div className="px-18 py-36 rounded-t-3xl bg-linear-to-b from-[#0B4858] to-[#81A6AF] relative">
+            <div className="absolute -top-10 left-0 w-full h-[500px]">
+              <SoftAurora
+                speed={1.3}
+                scale={1.2}
+                brightness={0.65}
+                color1="#78cfe3"
+                color2="#87b9d4"
+                noiseFrequency={1}
+                noiseAmplitude={3.5}
+                bandHeight={0.85}
+                bandSpread={1}
+                octaveDecay={0.12}
+                layerOffset={0.5}
+                colorSpeed={1}
+                enableMouseInteraction={false}
+                mouseInfluence={0.2}
+              />
+            </div>
+
             <div className="max-w-[1200px] px-8 mx-auto bg-red-500/0">
               <WhyAtomix />
             </div>
@@ -314,7 +333,26 @@ export default function LandingGradientV1Page() {
         </div>
 
         <div className="h-full mb-12">
-          <div className="px-18 py-36 rounded-3xl bg-linear-to-b from-[#0B4858] to-[#81A6AF]">
+          <div className="px-18 py-36 rounded-3xl bg-linear-to-b from-[#0B4858] to-[#81A6AF] relative overflow-hidden">
+            <div className="absolute -top-10 left-0 w-full h-[500px]">
+              <SoftAurora
+                speed={1.3}
+                scale={1.2}
+                brightness={0.65}
+                color1="#78cfe3"
+                color2="#87b9d4"
+                noiseFrequency={1}
+                noiseAmplitude={3.5}
+                bandHeight={0.85}
+                bandSpread={1}
+                octaveDecay={0.12}
+                layerOffset={0.5}
+                colorSpeed={1}
+                enableMouseInteraction={false}
+                mouseInfluence={0.2}
+              />
+            </div>
+
             <div className="max-w-[1200px] px-8 mx-auto bg-red-500/0">
               <WhyWorkWithUs />
             </div>
