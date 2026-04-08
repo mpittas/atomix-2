18776450e697269fccb01be72a9eb5ccb94614ee
mpittas <webpittas@gmail.com>
2026-04-08 17:07:23 +0300

    add cta and footer

--- app/landing-platform/page.tsx
+++ app/landing-platform/page.tsx
@@ -4,6 +4,8 @@ import MainHeroV3 from "@/components/MainHeroV3";
 import HeaderRounded from "@/components/headerRounded";
 import ScrollableTabsMission from "@/components/ScrollableTabsMission";
 import ScrollableTabsVision from "@/components/ScrollableTabsVision";
+import DefCta from "@/components/DefCta";
+import Footer from "@/components/Footer";
 
 export default function LandingPlatformPage() {
   return (
@@ -15,9 +17,14 @@ export default function LandingPlatformPage() {
 
       <ScrollableTabsVision />
 
-      <div className="px-3">
-        <div className="bg-green-900 min-h-screen rounded-3xl"></div>
+      <div id="def-cta" className="pt-30">
+        <DefCta
+          title="Build the Future of
+Asset-Backed Lending"
+        />
       </div>
+
+      <Footer />
     </div>
   );
 }
