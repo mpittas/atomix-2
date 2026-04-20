fd5e512d1f5860bc57ef1e177d427e79073f1491
mpittas <webpittas@gmail.com>
2026-04-20 15:54:32 +0300

    add content in Mission and Vision page for v1

--- app/mission-vision/page.tsx
+++ app/mission-vision/page.tsx
@@ -1,42 +1,18 @@
-"use client";
-
-import { useCallback, useEffect, useRef, useState } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
 import Header from "@/components/header";
-import MainHero from "@/main/MainHero";
-import MainProblemsTabs from "@/main/MainProblemsTabs";
-import MainPyramidWrapper from "@/main/MainPyramidWrapper";
-import MainSolutionsAnimation from "@/main/MainSolutionsAnimation";
-import MainBenefitsTabs from "@/main/benefits-tabs/";
-import MainTheMarket from "@/main/MainTheMarket";
-
-import DefHeading from "@/components/typo/DefHeading";
-import { Button as DefButton } from "@/components/ui";
-import IconBox from "@/components/IconBox";
-import { FaGlobe, FaUsers, FaMicrochip } from "react-icons/fa6";
-import DefCta from "@/components/DefCta";
 import Footer from "@/components/Footer";
-import SoftAurora from "@/components/backgrounds/SoftAurora";
-import ScrollableTabsv2 from "@/components/ScrollableTabsv2";
-import TechLimitations from "@/components/TechLimitations";
-import WhyAtomixLayout from "@/components/WhyAtomixLayout";
-import TheMarket from "@/components/TheMarket";
-import ScrollableTabsBenefits from "@/components/ScrollableTabsBenefits";
-import CurrentStatusDiagram from "@/components/CurrentStatusDiagram";
-import MainCurrentStatus from "@/main/MainCurrentStatus";
-import MainWhyWorkWithUs from "@/main/MainWhyWorkWithUs";
+import MissionVisionV1 from "@/components/mission-vision/MissionVisionV1";
+import MissionVisionV2 from "@/components/mission-vision/MissionVisionV2";
+import MissionVisionV3 from "@/components/mission-vision/MissionVisionV3";
 
 export default function MissionVisionSections() {
   return (
     <div className="html-wrap  relative">
       <div className="fixed inset-0 w-[1240px] left-1/2 -translate-x-1/2 border-l border-r border-dashed border-gray-950/30 z-[9999] pointer-events-none"></div>
       <Header />
-      <div className="mt-24 py-32 bg-red-500/20 page-wrapper">
-        <div className="max-w-[1260px] mx-auto px-4">
-          <h1>Hello, World!</h1>
-        </div>
+      <div className="mt-24 pt-4 px-12 bg-red-500/20 page-wrapper flex flex-col gap-y-12">
+        <MissionVisionV1 />
+        <MissionVisionV2 />
+        <MissionVisionV3 />
       </div>
       <Footer />
     </div>
--- components/mission-vision/MissionVisionV1.tsx
+++ components/mission-vision/MissionVisionV1.tsx
@@ -1,3 +1,49 @@
+import { Button as DefButton } from "@/components/ui";
+
+function MissionVisionBlock({
+  title,
+  description,
+  buttonText,
+  buttonLink,
+}: {
+  title: string;
+  description: string;
+  buttonText: string;
+  buttonLink: string;
+}) {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white flex flex-col items-start gap-y-8 w-xl">
+      <h2 className="text-[80px] font-normal font-bold uppercase leading-[1.05em]">
+        Our <br />
+        {title}
+      </h2>
+      <div className="w-full h-[1px] bg-white/16"></div>
+      <div className="text-xl">{description}</div>
+      <DefButton size="small" href={buttonLink}>
+        {buttonText}
+      </DefButton>
+    </div>
+  );
+}
+
 export default function MissionVisionV1() {
-  return <div>Mission Vision V1</div>;
+  return (
+    <div className="min-h-[calc(100vh-126px)] rounded-3xl bg-linear-to-b from-[#0B4858] via-[#1e5360] to-[#0B4858] relative overflow-hidden flex flex-col justify-center items-center">
+      <div className="relative">
+        <MissionVisionBlock
+          title="Mission"
+          description="Rebuild UK property lending. Start with bridging. Extend into SME CRE term loans — same infrastructure, no rebuild."
+          buttonText="Learn More"
+          buttonLink="#"
+        />
+
+        <MissionVisionBlock
+          title="Vision"
+          description="Rebuild UK property lending. Start with bridging. Extend into SME CRE term loans — same infrastructure, no rebuild."
+          buttonText="Learn More"
+          buttonLink="#"
+        />
+      </div>
+    </div>
+  );
 }
